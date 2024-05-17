690b88d99b885ac0d391c9386090445cac52ac91
shreeshailaya <s.r.vitkar55@gmail.com>
2024-05-18 01:07:52 +0530

    SQL connector added with OTl flag

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 import json
 from decouple import config
 from api_call import ApiCall
+from operations import initial_site_registration
 api_call = ApiCall()
 if __name__ == '__main__':
     env_variables = config("SITES_DATA")
@@ -8,4 +9,5 @@ if __name__ == '__main__':
     for key in env_variables:
         url = env_variables[key]["url"]
         tags = env_variables[key]["tags"]
-        api_call.makeApiRequest(url=url, tags=tags)
+        site_id, otl = initial_site_registration(site_data=env_variables, url=url, tags=tags)
+        api_call.makeApiRequest(url=url, tags=tags, site_id=site_id, otl = otl)
